eea022a027a356cdce4a6a842f5ec9bfde292059
Gleb Bondarenko <renkornator@gmail.com>
2023-07-02 10:34:06 +0300

    version 3

--- validator.go
+++ validator.go
@@ -1,77 +1,50 @@
 package main
 
 /*
-	Parentheses validator based on reversed linked list with
+	Parentheses validator based on reversed linked list
 */
 
-var parenthesisDict = map[rune]rune{
-	'(': ')',
-	'[': ']',
-	'{': '}',
+type ListNode struct {
+	value rune
+	next  *ListNode
 }
 
-var closingMap = map[rune]bool{
-	')': true,
-	']': true,
-	'}': true,
-}
-
-type closingRune struct {
-	val  rune
-	next *closingRune
-}
-
-type List struct {
-	head *closingRune
-}
-
-// inserts new closing rune at the head of the list
-func (l *List) Insert(r rune) {
-	newHead := &closingRune{val: r, next: nil}
-	if l.head == nil {
-		l.head = newHead
-	} else {
-		newHead.next = l.head
-		l.head = newHead
+func push(stack **ListNode, value rune) {
+	node := &ListNode{
+		value: value,
+		next:  *stack,
 	}
+	*stack = node
 }
 
-// checks if rune at the head of the list is equal incoming rune
-// if so, pops the head and returns true
-// otherwise returns false
-func (l *List) Check(r rune) bool {
-	if l.head == nil {
-		return false
+func pop(stack **ListNode) rune {
+	if *stack == nil {
+		return 0
 	}
-	if l.head.val == r {
-		if l.head.next != nil {
-			l.head = l.head.next
-		} else {
-			l.head = nil
-		}
-		return true
-	}
-	return false
+	value := (*stack).value
+	*stack = (*stack).next
+	return value
 }
 
 func isValid(s string) bool {
-	if len(s) == 0 {
-		return true
+	var stack *ListNode
+
+	bracketPairs := map[rune]rune{
+		')': '(',
+		'}': '{',
+		']': '[',
 	}
 
-	l := &List{}
-
-	for _, symb := range s {
-		if nextClosingRune, ok := parenthesisDict[symb]; ok {
-			l.Insert(nextClosingRune)
-		} else {
-			if _, ok := closingMap[symb]; ok {
-				if !l.Check(symb) {
-					return false
-				}
+	for _, char := range s {
+		switch char {
+		case '(', '{', '[':
+			push(&stack, char)
+		case ')', '}', ']':
+			if pop(&stack) != bracketPairs[char] {
+				return false
 			}
 		}
 	}
 
-	return l.head == nil
+	return stack == nil
 }
